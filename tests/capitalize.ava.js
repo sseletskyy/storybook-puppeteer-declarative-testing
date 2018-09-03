0933e2d71b0cf10638a200c6f4e66a3691da56c5
Sergey Seletskyy <sseletskyy@gmail.com>
2018-09-03 13:17:45 +0300

    fix issue with capitalizing
    to support PascalCase file names;
    also add peer dependency for @storybook/react

--- tests/capitalize.ava.js
+++ tests/capitalize.ava.js
@@ -3,7 +3,7 @@ const { capitalize } = require('../lib/story-index-generator')
 
 test('Capitalize', (t) => {
   t.is(capitalize('abc'), 'Abc')
-  t.is(capitalize('ABc'), 'Abc')
-  t.is(capitalize('aBC'), 'Abc')
-  t.is(capitalize('ABC'), 'Abc')
+  t.is(capitalize('ABc'), 'ABc')
+  t.is(capitalize('aBC'), 'ABC')
+  t.is(capitalize('ABC'), 'ABC')
 })
--- tests/to-pascal-case.ava.js
+++ tests/to-pascal-case.ava.js
@@ -4,7 +4,7 @@ const { toPascalCase } = require('../lib/story-index-generator')
 test('toPascalCase', (t) => {
   t.is(toPascalCase('abc'), 'Abc')
   t.is(toPascalCase('abc-def'), 'AbcDef')
-  t.is(toPascalCase('aBC-dEF'), 'AbcDef')
+  t.is(toPascalCase('aBC-dEF'), 'ABCDEF') // keep case of the rest chars
   t.is(toPascalCase('abc_def'), 'AbcDef')
-  t.is(toPascalCase('ABC DEF'), 'AbcDef')
+  t.is(toPascalCase('AbC DEf'), 'AbCDEf')
 })
