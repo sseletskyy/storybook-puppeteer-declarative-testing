00d11aba2d26000fedd949469ff2ebbbc02dee78
Sergiy Seletskyy <sseletskyy@apixio.com>
2019-03-05 15:01:56 +0200

    structure custom test declarations

--- config-templates/test-declarations.js
+++ config-templates/test-declarations.js
@@ -1,22 +1,5 @@
-const testCustomDeclarativeTest = (fixture) => {
-  const { customDeclarativeTest } = (fixture && fixture.spdt) || {}
-  let selector
-  let value
-  if (typeof customDeclarativeTest === 'string') {
-    selector = 'h1'
-    value = customDeclarativeTest
-  }
-  if (!selector || !value) {
-    return null
-  }
-  return `
-    it('should find component matching selector [${selector}] with value ${value}', async () => {
-      const components = await iFrame.$$('${selector}')
-      const expected = 1
-      expect(components).toHaveLength(expected)
-    })`
-}
+const { testH1 } = require('./custom-declarations/test-h1')
 
 module.exports = {
-  customDeclarativeTest: testCustomDeclarativeTest,
+  testH1,
 }
