e6e698805f4168137c879a31044084ba4d097aa7
Sergiy Seletskyy <sseletskyy@apixio.com>
2019-03-06 11:28:17 +0200

    fix test-h1 example; add circle ci configuration to run tests

--- config-templates/custom-declarations/test-h1.js
+++ config-templates/custom-declarations/test-h1.js
@@ -1,22 +1,22 @@
-const testH1 = (fixture) => {
-  const { customDeclarativeTest } = (fixture && fixture.spdt) || {}
+const declarationTestH1 = (fixture) => {
+  const { testH1 } = (fixture && fixture.spdt) || {}
   let selector
   let value
-  if (typeof customDeclarativeTest === 'string') {
+  if (typeof testH1 === 'string') {
     selector = 'h1'
-    value = customDeclarativeTest
+    value = testH1
   }
   if (!selector || !value) {
     return null
   }
   return `
-    it('should find component matching selector [${selector}] with value ${value}', async () => {
-      const components = await iFrame.$$('${selector}')
-      const expected = 1
-      expect(components).toHaveLength(expected)
+    it('testH1: should find component matching selector [${selector}] with value ${value}', async () => {
+      const components = await iFrame.$$eval('[id=root] ${selector}', elements => elements.map(e => e.innerText))
+      const expected = '${value}'
+      expect(components).toContain(expected)
     })`
 }
 
 module.exports = {
-  testH1,
+  testH1: declarationTestH1,
 }
